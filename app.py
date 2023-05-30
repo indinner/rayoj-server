7a150763f7a92b22ac4566e1ec7dc7750624ee0a
indinner <520244@qq.com>
2023-05-30 10:00:04 +0800

    1

--- app.py
+++ app.py
@@ -12,45 +12,6 @@ app = Flask(__name__)
 # 启动Ray.
 ray.init('ray://43.139.117.81:10001')
 
-
-# ray.init(address='auto', _node_ip_address='119.45.71.218')
-# ray.init(address='119.45.71.218:6379',_redis_password='5241590000000000')
-
-@ray.remote
-class MyActor:
-    def __init__(self):
-        self.value = 0
-
-    def ray_oj(self, data):
-        code = data['code']  # 执行代码
-        input_case = data['input_case']  # 输入样例,数组
-        output_case = data['output_case']  # 输出样例,数组
-        time = data['time']  # 时间限制
-        memory = data['memory']  # 空间限制
-        language = data['language']  # 语言
-        result_type = data['result_type']  # 返回结果类型，json str
-        random_id = str(uuid.uuid1())
-        subprocess.call('mkdir /testcase/' + random_id, shell=True)  # 创建一个测试文件夹
-        subprocess.call('echo "' + code + '" > /testcase/' + random_id + '/test.py', shell=True)  # 创建待测试文件
-        for i in range(0, len(input_case)):  # 创建测试用例文件
-            subprocess.call('echo "' + input_case[i] + '" > /testcase/' + random_id + '/' + str(i) + '.in', shell=True)
-            subprocess.call('echo "' + output_case[i] + '" > /testcase/' + random_id + '/' + str(i) + '.out',
-                            shell=True)
-            cmd = ''
-            if language == 'python':
-                cmd = "python /home/acm-judge-module/judge/judge.py --language python3 --languageConfig /home/acm-judge-module/judge/language/ --file /testcase/" + random_id + "/test.py --time " + str(
-                    time) + " --memory " + str(
-                    memory) + " --testDir /testcase/" + random_id + " --mode entire --type " + result_type + " --delete false --codeResultDir " + "/testcase/" + random_id,
-        res = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE, text=True)
-        (out, err) = res.communicate()
-        pass
-        return out
-
-
-my_actor = MyActor.remote()
-
-
 @app.route('/oj_run_v2', methods=['POST'])
 @cross_origin(origins="*")
 def oj_run_v2():  # put application's code here
@@ -115,15 +76,23 @@ def ray_oj(data):
     result_type = data['result_type']  # 返回结果类型，json str
     random_id = str(uuid.uuid1())
     subprocess.call('mkdir /testcase/' + random_id, shell=True)  # 创建一个测试文件夹
-    subprocess.call('echo "' + code + '" > /testcase/' + random_id + '/test.py', shell=True)  # 创建待测试文件
+    filename = ''
+    if language == 'python':
+        filename = 'test.py'
+    elif language == 'c++':
+        filename = 'Main.cpp'
+    subprocess.call('echo "' + code + '" > /testcase/' + random_id + '/' + filename, shell=True)  # 创建待测试文件
     for i in range(0, len(input_case)):  # 创建测试用例文件
         subprocess.call('echo "' + input_case[i] + '" > /testcase/' + random_id + '/' + str(i) + '.in', shell=True)
         subprocess.call('echo "' + output_case[i] + '" > /testcase/' + random_id + '/' + str(i) + '.out', shell=True)
-        cmd = ''
+        language_type = ''
         if language == 'python':
-            cmd = "python /home/acm-judge-module/judge/judge.py --language python3 --languageConfig /home/acm-judge-module/judge/language/ --file /testcase/" + random_id + "/test.py --time " + str(
-                time) + " --memory " + str(
-                memory) + " --testDir /testcase/" + random_id + " --mode entire --type " + result_type + " --delete false --codeResultDir " + "/testcase/" + random_id,
+            language_type = 'python3'
+        elif language == 'c++':
+            language_type = 'cpp'
+        cmd = "python /home/acm-judge-module/judge/judge.py --language " + language_type + " --languageConfig /home/acm-judge-module/judge/language/ --file /testcase/" + random_id + "/" + "Main" + " --time " + str(
+            time) + " --memory " + str(
+            memory) + " --testDir /testcase/" + random_id + " --mode entire --type " + result_type + " --delete false --codeResultDir " + "/testcase/" + random_id
     res = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
                            stdout=subprocess.PIPE, text=True)
     # print(cmd)
